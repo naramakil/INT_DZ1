fea6a94f25baeec516c7b0fcdad4393f62ade662
Naramakil <naramakil@gmail.com>
2025-05-05 16:13:37 +0300

    Code reformating

--- src/main/java/ru/netology/INT_1/Todos.java
+++ src/main/java/ru/netology/INT_1/Todos.java
@@ -4,14 +4,6 @@ public class Todos {
 
     private Task[] tasks = new Task[0]; // <- тут будут все задачи
 
-    /**
-     * Вспомогательный метод для имитации добавления элемента в массив
-     *
-     * @param current Массив, в который мы хотим добавить элемент
-     * @param task    Элемент, который мы хотим добавить
-     * @return Возвращает новый массив, который выглядит как тот, что мы передали,
-     * но с добавлением нового элемента в конец
-     */
     private Task[] addToArray(Task[] current, Task task) {
         Task[] tmp = new Task[current.length + 1];
         for (int i = 0; i < current.length; i++) {
@@ -21,12 +13,7 @@ public class Todos {
         return tmp;
     }
 
-    /**
-     * Метод добавления задачи в список дел
-     *
-     * @param task Добавляемая задача
-     */
-    public void add(Task task) { // <- вот здесь в параметре может лежать объект и вида SimpleTask, и вида Epic, и вида Meeting
+    public void add(Task task) {
         tasks = addToArray(tasks, task);
     }
 
